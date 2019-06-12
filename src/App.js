8af6631ce9ae9cd0290308ed9df8408a35a189e4
BBDO\rrajaona <rajaonarivelor@proximity.bbdo.fr>
2019-06-12 15:22:27 +0400

    [Calculator] : Adding effect on project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 import Calculator from './calculator'
 import data from './calculator_config.json'
+import { Bounce, Zoom } from 'react-reveal'
 
 class App extends Component {
 
@@ -22,14 +23,33 @@ class App extends Component {
   render () {
     const { calculator } = this.state
     return (
-      <div className = "page-wrapper">
+      <div className = "l-layout">
         <header className = "l-heading">
-          <img src = {logo} className = "l-heading-logo" alt = "logo" />
+          <div className = "l-page-container">
+            <img src = {logo} className = "l-heading-logo" alt = "logo" />
+            React Calculator Demo
+          </div>
         </header>
         <main className = "l-main">
-          <Calculator calculatorInstance = {calculator} />
+          <div className = "l-page-container">
+            <Bounce left>
+              This is an example application with React JS.
+              <ul className = "l-list">
+                <li className = "l-list-item"> Layout for page can be found in <code> /App.js </code></li>
+                <li className = "l-list-item"> Module Calculator has been implement in <code> /calculator/index.js </code></li>
+                <li className = "l-list-item"> We implement a submodule for button in <code> /calculator/buttons/CalculatorButton.js </code></li>
+                <li className = "l-list-item"> Calculator can be configured in <code> calculator_config.json </code></li>
+              </ul>
+            </Bounce>
+            <Zoom top>
+              <Calculator calculatorInstance = {calculator} />
+            </Zoom>
+          </div>
         </main>
         <footer className = "l-footer">
+          <div className = "l-page-container">
+            © 2019 – RAJAONARIVELO Ravoatra Mihaja – Front End Developer | All Rights Reserved
+          </div>
         </footer>
       </div>
     )
